03f4c582bf283d39d86de80047bf4570866531c9
ketan <ketan.kanhasoft@gmail.com>
2024-05-09 11:58:50 +0530

    feat: make clickable active buttons

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,15 +1,25 @@
-import { FunctionComponent, PropsWithChildren } from "react";
+import { MouseEventHandler, FunctionComponent, PropsWithChildren } from "react";
 import classNames from "classnames";
 
-type ButtonProps = {};
+// Define type for ButtonProps
+type ButtonProps = {
+  onClick: MouseEventHandler<HTMLButtonElement>;
+  active: boolean;
+};
 
+// Define Button component as a FunctionComponent with ButtonProps as props
 export const Button: FunctionComponent<PropsWithChildren<ButtonProps>> = ({
   children,
+  onClick,
+  active,
 }) => {
   return (
     <button
       type="button"
-      className={classNames("px-2 py-1 border border-black")}
+      className={classNames(
+        `px-2 py-1 border border-black ${active ? "bg-[#5351b3] text-[#f2f2ff]" : ""}`,
+      )}
+      onClick={onClick}
     >
       {children}
     </button>
--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,17 +1,25 @@
-import { FunctionComponent, PropsWithChildren } from "react";
+// import dependancies
+import { FunctionComponent, PropsWithChildren, useState } from "react";
 import { Inter } from "next/font/google";
 import classNames from "classnames";
 import { Button } from "@/components/Button";
 import { Person } from "@/utils/common/person";
 
+// Initialize Inter font with Latin subset
 const inter = Inter({ subsets: ["latin"] });
 
+// Define props interface for MainLayout component
 type MainLayoutProps = {};
 
+// MainLayout component definition
 export const MainLayout: FunctionComponent<
   PropsWithChildren<MainLayoutProps>
 > = () => {
+  // State to manage active button ID
+  const [activeBtnId, setActiveBtnId] = useState<string>("");
+
   return (
+    // Main container with flex layout, centered content
     <main
       className={classNames(
         inter.className,
@@ -19,11 +27,18 @@ export const MainLayout: FunctionComponent<
         "flex flex-col justify-center items-center",
       )}
     >
+      {/* Container for buttons with flex layout */}
       <div className={classNames("flex gap-2")}>
+        {/* Mapping through Person data to render buttons */}
         {Object.values(Person).map((person) => (
-          <Button key={person}>{person}</Button>
+          <Button
+            key={person}
+            onClick={() => setActiveBtnId(person)}
+            active={activeBtnId === person}
+          >
+            {person} {/* Displaying person's name as button label */}
+          </Button>
         ))}
-        <button />
       </div>
     </main>
   );
